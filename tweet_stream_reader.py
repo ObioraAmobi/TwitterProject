0e68cf5c66dae6da3b41d816edeb7dea40c1d5e8
Obiora Amobi <obiora.amobi@gmail.com>
2016-03-15 16:02:39 +0000

    Tweet Stream modified

--- tweet_stream_reader.py
+++ tweet_stream_reader.py
@@ -30,3 +30,31 @@ statuses['country'] = map(lambda status: status['place']['country'] if status['p
 
 # get each tweet language and the count of its appearance (not to be confused with programming languages
 tweets_by_lang = statuses['lang'].value_counts()
+
+# create our drawing space/window (figure)
+fig = plt.figure()
+
+# add a plot area for our data on the figure - 1,1,1 means a single chart/graph
+ax = fig.add_subplot(1, 1, 1)
+
+# style the axes and labels of our plot
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Tweet Languages', fontsize=15)
+ax.set_ylabel('Number of tweets', fontsize=15)
+ax.xaxis.label.set_color('#666666')
+ax.yaxis.label.set_color('#666666')
+ax.tick_params(axis='x', colors='#666666')
+ax.tick_params(axis='y', colors='#666666')
+#style the title
+ax.set_title('Top 10 languages', fontsize=15, color='#666666')
+
+# plot the top 10 tweet languages and appearance count using a bar chart
+tweets_by_lang[:10].plot(ax=ax, kind='bar', color='#FF7A00')
+
+# color the spines (borders)
+for spine in ax.spines.values():
+    spine.set_edgecolor('#666666')
+
+# render the graph
+plt.show()
